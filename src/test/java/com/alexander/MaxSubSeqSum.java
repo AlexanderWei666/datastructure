cecaa91a0f9deb01c64aacdee28e6b0fe1b39c96
AlexanderWei <alexanderwei0013@gmail.com>
2018-08-27 14:16:02 +0800

    init

---
+++ src/test/java/com/alexander/MaxSubSeqSum.java
@@ -0,0 +1,16 @@
+package com.alexander;
+
+public class MaxSubSeqSum{
+    public static void main(String[] args) {
+        
+    }
+
+    //todo
+    public void time(){}
+    public void method1(){}
+    public void method2(){}
+    public void method3(){}
+    public void method4(){}
+
+
+}
